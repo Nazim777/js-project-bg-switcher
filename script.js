de8371ed59868660917b0ab943263fbe03f3a3a4
Nazim777 <iamnazimhossain@gmail.com>
2022-07-20 19:57:46 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let crimson = document.getElementById("crimson");
+crimson.addEventListener("click",function(){
+    document.body.style.background= "crimson";
+})
+let white = document.getElementById("white");
+white.addEventListener("click",function(){
+    document.body.style.background = "white";
+})
+let darkcyan = document.getElementById("darkcyan");
+darkcyan.addEventListener("click",function(){
+    document.body.style.background = "darkcyan";
+})
+let orange = document.getElementById("orange");
+orange.addEventListener("click",function(){
+    document.body.style.background = "orange";
+   
+})
+
+
